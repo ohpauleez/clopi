f47cbde9a6a3b9d069edfddf572b87b5e5cc0055
Paul deGrandis <paul.degrandis@gmail.com>
2011-03-04 08:39:14 -0800

    Rename and clojars refactor; sorting/ordering by total dependency count

--- src/clopi/core.clj
+++ src/clopi/core.clj
@@ -2,20 +2,11 @@
   (:import (java.util.zip GZIPInputStream)
            (java.io StringReader)))
 
-(def *feed-archive-url* "http://clojars.org/repo/feed.clj.gz")
 
 (defn gunzip
   "Unzip a gzip archive into an input stream"
   [archive]
   (GZIPInputStream. (clojure.java.io/input-stream archive)))
-
-(defn istream->urls
-  "Resolve the feed input stream lazily, generating a set of all the URLS"
-  [istream]
-  (let [rdr (clojure.java.io/reader istream)]
-    (reduce (fn [url-set line]
-              (let [jar-map (read-string line)]
-                (conj url-set (get jar-map :url "")))) #{} (line-seq rdr))))
 
 (defn project->map
   "Process a project.clj, generating a map of the data"
@@ -29,7 +20,7 @@
       (read-string project-map-str))
     {}))
 
-(defn fetch-url
+(defn fetch-proj-url
   "Fetch the dependencies and dev-dependencies of a project.clj url"
   [url]
   (let [rdr (try
@@ -44,18 +35,26 @@
   "Fetch the dependencies and dev-dependencies of a github host project"
   [url]
   (try
-    (fetch-url (.replaceFirst (str url "/raw/master/project.clj") "http:" "https:"))
+    (fetch-proj-url (.replaceFirst (str url "/raw/master/project.clj") "http:" "https:"))
     (catch Exception e {})))
+
+(defn dispatch-url
+  "Given a url, dispatch to the correct fetch-url call"
+  [url]
+  (condp .contains url
+    "github"  (fetch-github url)
+    ""        (fetch-proj-url url)))
  
 (defn count-dep-vec
   [dep-vector stats-map-start]
   (reduce (fn [stats-map [proj-index version-str]]
-            (let [proj-index (str proj-index)]
+            (let [proj-index (str proj-index)
+                  version-str (.replaceFirst (str version-str) "[" "")] ;sometime version come in as "[1.2.0"
               (update-in stats-map [proj-index version-str] (fnil inc 0))))
           stats-map-start dep-vector))
 
 (defn count-deps
-  "Create a map of all artifacts, and how many times someone has a dependency on a specific version"
+  "Create a map of all artifacts, and how many times someone has a dependency on a specific version (depmap)"
   [deps]
   (reduce (fn [stats-map dep-map]
             (try
@@ -63,18 +62,10 @@
                                    (get dep-map :dev-dependencies [])) stats-map)
               (catch Exception e (do (println dep-map) stats-map))))
           (sorted-map) deps))
- 
-#_(defn process-gzip
-  "This needs to be done, but will be done via the project files."
-  [istream]
-  (let [rdr (clojure.java.io/reader istream)]
-    (loop [stat-map (transient {})
-           line (.readLine rdr)]
-      (if line
-        (let [jar-map (read-string line)
-              url (jar-map :url)]
-          (condp .contains url
-            "github" (fetch-github url)
-            "bitbucket" (fetch-bitbucket url)
-            ""))))))
+
+(defn ranked-depvec
+  "Given a depmap, return a new vector [[XX 'artifact']...] sorted by the total dependency count, XX (sum of all versions)"
+  [depmap]
+  (sort #(> (second %1) (second %2))
+        (map (fn [[artifact-name val-vec]] [artifact-name (apply + (vals val-vec))]) depmap)))
 
