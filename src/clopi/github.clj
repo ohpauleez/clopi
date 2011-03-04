d165bedfd3bd2758bdfe0677097908fee6535b28
Paul deGrandis <paul.degrandis@gmail.com>
2011-03-04 08:35:48 -0800

    Renaming refactor, based on clojars refactor

--- src/clopi/github.clj
+++ src/clopi/github.clj
@@ -10,8 +10,8 @@
     (reduce (fn [res-vec page]
               (into res-vec (do (Thread/sleep 1000) (github/search-repos auth "clojure" :language "Clojure" :start-page page)))) [] (range 1 50))))
 
-(defn results->urls
-  "Take a search results vector, and return all the urls"
+(defn repos->urls
+  "Take a search results vector (as returned by clojure-repos), and return all the urls"
   [results]
   (into #{} (map #(get %1 :url "") results)))
 
