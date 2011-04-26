87f0ed747e63efcc2d1d147f493f6a4fe19d85f6
Paul deGrandis <paul.degrandis@gmail.com>
2011-04-26 11:51:23 -0700

    WIP, searching clojars, and augmenting search with dep information

--- src/clopi/search.clj
+++ src/clopi/search.clj
@@ -6,7 +6,15 @@
   [query]
   (let [search-url (str "http://clojars.org/search?q=" query "&format=json")
         rdr (clojure.java.io/reader search-url)
-        res (:results (json/parse-string (apply str (line-seq rdr))))]))
+        resp (json/parse-string (apply str (line-seq rdr)))]
+    (resp "results")))
+
+(defn- clojars->depmap [m]
+  (let [gn (m "group_name")
+        jn (m "jar_name")]
+    (if (= gn jn)
+      (str gn)
+      (str gn "/" jn))))
 
 (defn augment-search
   "Perform a search query, but augment the results with a given depmap"
@@ -15,7 +23,7 @@
      #_(sort #() res)))
   ([query depmap]
    (let [res (clojars-search query)
-         res_aug (reduce #(conj %1 (assoc %2 "deps" (depmap (%2 "SOME NAME HERE")))) [] res)])))
-
-
+         ;; TODO depmap is currently a seq of vectors eg: ["aleph" {"0.1.5-SNAPSHOT" 2, "0.1.0-SNAPSHOT" 1}], and below doesn't contain/use version info
+         res_aug (reduce #(conj %1 (assoc %2 "deps" (depmap (clojars->depmap %2)))) [] res)]
+     res_aug)))
 
