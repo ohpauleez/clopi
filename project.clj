89601717701302efa5e76823f5be547f070ee634
Paul deGrandis <paul.degrandis@gmail.com>
2011-04-26 23:36:38 -0700

    Project tweaks

--- project.clj
+++ project.clj
@@ -8,7 +8,7 @@
   :dependencies [;[org.clojure/clojure "1.3.0-master-SNAPSHOT"]
                  [org.clojure/clojure "1.2.1"]
                  ;[org.clojure/clojure "1.3.0-alpha6"]
-                 [clj-github "1.0.1-SNAPSHOT"]
+                 [clj-github "1.0.1"]
                  [clj-json "0.3.1"]]
   :dev-dependencies [[vimclojure/server "2.3.0-SNAPSHOT"]
                      [org.clojure/clojure-contrib "1.2.0"]
@@ -23,7 +23,7 @@
                      [com.stuartsierra/lazytest "2.0.0-SNAPSHOT"]]
   :repositories {"stuartsierra-releases" "http://stuartsierra.com/maven2"
                  "stuartsierra-snapshots" "http://stuartsierra.com/m2snapshots"}
-  ;:hooks  [leiningen.hooks.cdt]
+  :hooks  [leiningen.hooks.cdt]
   ;:warn-on-reflection true
   ;:jvm-opts ["-Xmx1g"]
   :repl-init-script "script/repl_init.clj")
